ffa5c83aa810cccfe621e64a402e546a4cd12227
Tung Nguyen <xuantung.1998@gmail.com>
2022-02-24 21:09:25 +0700

    dob_analytics update problem

--- 002_tool_study/005_dob_analytics/practice_05.py
+++ 002_tool_study/005_dob_analytics/practice_05.py
@@ -3,5 +3,6 @@ Date of birth analytics
 Given: a csv data with studentId and DoB of IU student
 Expect: give some summary about IU student date of birth
 - DoB month distribution: how many student born in Jan, Feb, ...?
+- DoB year distribution: how many student born in 1998, 1999, ...?
 """
 import csv
